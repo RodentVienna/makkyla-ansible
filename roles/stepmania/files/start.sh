60f283565f32191d92b3d6ea5ccec8225af6a3af
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2017-09-17 22:29:00 +0300

    Start Stepmania when DE starts

--- roles/stepmania/files/start.sh
+++ roles/stepmania/files/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# wait until pulseaudio has started, pasuspender doesn't work otherwise
+while [ -z "$(pidof pulseaudio)" ]; do
+  sleep 0.5
+done
+
 pasuspender /stepmania/stepmania
 
 # wait until enter is pressed
