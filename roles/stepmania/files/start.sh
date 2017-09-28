395c4b20aeafdd68f13c04c934dd6c55dd18f564
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2017-09-28 20:11:33 +0300

    Hide mouse cursor if not used

--- roles/stepmania/files/start.sh
+++ roles/stepmania/files/start.sh
@@ -5,7 +5,12 @@ while [ -z "$(pidof pulseaudio)" ]; do
   sleep 0.5
 done
 
+# hide mouse cursor if not moved
+trap 'pkill unclutter' EXIT
+unclutter -idle 1 -root &
+
 pasuspender /stepmania/stepmania
 
 # wait until enter is pressed
 read -p "Press enter to continue"
+
