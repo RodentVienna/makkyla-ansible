7b9f633af7bc7d4556642c07b9ab4d99adb21491
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2017-12-26 17:54:36 +0200

    Attempt to clone display for capturing

--- roles/stepmania/files/start.sh
+++ roles/stepmania/files/start.sh
@@ -14,6 +14,9 @@ xset s off
 # hide mouse cursor if not moved
 unclutter -idle 1 -root &
 
+# try to clone displays for streming – but if it's not possible for some reason , that's fine as well
+xrandr --output HDMI-0 --same-as DVI-D-0 --mode 1280x720 --scale-from 1920x1080 || true
+
 # force vsync off
 export __GL_SYNC_TO_VBLANK=0
 
